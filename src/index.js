20b32f5376546299eb802511df62c94508b88d5e
松田尚也 <hero.puma@gmail.com>
2020-03-09 18:47:36 +0900

    [add]entry point for slack event api

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { createEventAdapter } = require("@slack/events-api");
+
+const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
+const port = process.env.PORT || 3000;
+
+const slackEvents = createEventAdapter(slackSigningSecret);
+
+const app = express();
+
+app.use("/receive/slack_event", slackEvents.requestListener());
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Listening for events on ${port}`);
+});
