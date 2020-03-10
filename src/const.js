13713be28bab365086d2ae68f59e09fe75058675
松田尚也 <hero.puma@gmail.com>
2020-03-10 19:06:39 +0900

    [add]fetch message info by slack-event

--- src/const.js
+++ src/const.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 
-const constObj = { SLACK_SIGNING_SECRET: process.env.SLACK_SIGNING_SECRET };
+const constObj = {
+  SLACK_SIGNING_SECRET: process.env.SLACK_SIGNING_SECRET,
+  SLACK_TOKEN: process.env.SLACK_TOKEN
+};
 
 Object.freeze(constObj);
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { createEventAdapter } = require("@slack/events-api");
-const { SLACK_SIGNING_SECRET } = require("./const");
+const { WebClient } = require("@slack/web-api");
+const { SLACK_SIGNING_SECRET, SLACK_TOKEN } = require("./const");
 
 const port = process.env.PORT || 3000;
+
+const web = new WebClient(SLACK_TOKEN);
 
 const slackEvents = createEventAdapter(SLACK_SIGNING_SECRET);
 
@@ -11,7 +14,17 @@ const app = express();
 app.use("/receive/slack_event", slackEvents.requestListener());
 app.use(express.json());
 
-slackEvents.on("reaction_added", event => console.log(event));
+slackEvents.on("reaction_added", async event => {
+  web.conversations
+    .history({
+      channel: event.item.channel,
+      latest: event.item.ts,
+      limit: 1,
+      inclusive: true
+    })
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+});
 slackEvents.on("reaction_removed", event => console.log(event));
 slackEvents.on("error", error => console.error(error));
 
