e35fb18b24c9a59ed8d2970c9ca171a4c421b8ff
松田尚也 <hero.puma@gmail.com>
2020-03-22 20:02:00 +0900

    [add]Read and Write Oauth token

---
+++ src/token.js
@@ -0,0 +1,18 @@
+const { readJsonFile, writeJsonFile } = require("./jsonFile");
+
+module.exports.readToken = async () => {
+  const result = await readJsonFile("../token.json").then(token => {
+    if (!token.access_token || !token.refresh_token)
+      throw new Error("not found token");
+    return token;
+  });
+  return {
+    accessToken: result.access_token,
+    refreshToken: result.refresh_token
+  };
+};
+
+module.exports.writeToken = async (oldToken, json) => {
+  const registerToken = { ...oldToken, ...json };
+  writeJsonFile("../token.json", registerToken);
+};
