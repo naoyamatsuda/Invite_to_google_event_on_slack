ae812c91c06c97274a28dd3f7cabb0bc83ecab45
松田尚也 <hero.puma@gmail.com>
2020-03-22 20:00:46 +0900

    [add]Function for json file

---
+++ src/jsonFile.js
@@ -0,0 +1,47 @@
+const fsPromises = require("fs").promises;
+require("../oauthCredential");
+
+const jsonparseAsync = json => {
+  return new Promise((resolve, reject) => {
+    if (!json) return reject(Error("undeifned json"));
+    try {
+      return resolve(JSON.parse(json));
+    } catch (e) {
+      return reject(e);
+    }
+  });
+};
+
+module.exports.readJsonFile = async path => {
+  const tokenFile = await fsPromises.readFile(path).catch(e => {
+    throw e;
+  });
+
+  const result = await jsonparseAsync(tokenFile)
+    .then(json => {
+      if (!json) throw new Error("json file is empty");
+      if (!Object.keys(json).length) throw new Error("json is Undefined");
+      return json;
+    })
+    .catch(e => {
+      console.log(e);
+      throw e;
+    });
+
+  return result;
+};
+
+module.exports.writeJsonFile = async (path, json) => {
+  let jsonString;
+  try {
+    jsonString = JSON.stringify(json);
+  } catch (e) {
+    console.log(e);
+    throw new Error("json sgtinfy error");
+  }
+  const result = await fsPromises.writeFile(path, jsonString).catch(err => {
+    console.log(err);
+    throw new Error(err);
+  });
+  return result;
+};
