dd07bd2ca5852f35a45301065031dc304b9a835e
松田尚也 <hero.puma@gmail.com>
2020-03-27 16:38:58 +0900

    [add]Google Api Base URL

--- src/const.js
+++ src/const.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 
 const constObj = {
   SLACK_SIGNING_SECRET: process.env.SLACK_SIGNING_SECRET,
-  SLACK_TOKEN: process.env.SLACK_TOKEN
+  SLACK_TOKEN: process.env.SLACK_TOKEN,
+  GOOGLE_API_BASE_URL: "https://www.googleapis.com/calendar/v3"
 };
 
 Object.freeze(constObj);
